4d997cc75400c7ebf248dc8a2196a216dfaed2d6
Federico Di Iorio <federico_metallica@hotmail.com>
2024-12-04 17:32:12 -0300

    Primeros pasos

---
+++ src/main/java/com/coderhouse/interfaces/DAOInterface.java
@@ -0,0 +1,17 @@
+package com.coderhouse.interfaces;
+
+import java.util.List;
+
+public interface DAOInterface<reqDTO, resDTO> {
+
+    List<resDTO> getAll();
+
+    resDTO getById(Long id);
+    
+    resDTO save(reqDTO object);
+
+    resDTO update(Long id, reqDTO object) throws Exception;
+
+    void delete(Long id);
+    
+}
